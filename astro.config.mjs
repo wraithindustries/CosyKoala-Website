d15fef5a464b997c29aa4ed6c66fd1efdbf811a3
Brendan Thompson <github@blt.is>
2024-01-26 09:54:02 +1100

    feat: add mdx
    Signed-off-by: Brendan Thompson <github@blt.is>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cosykoala.app',
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
