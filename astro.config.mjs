887824069ab7337593352cefa8cb0943f0389197
Brendan Thompson <github@blt.is>
2024-01-27 10:25:15 +1100

    fix: remove custom dirs for config
    Signed-off-by: Brendan Thompson <github@blt.is>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  root: 'src',
   site: 'https://cosykoala.app',
   integrations: [tailwind(), mdx()]
 });
